7b53233a88e0d6eac5a8ea7263b591b9d62bcf29
lu-jiayi <jlg363@stanford.edu>
2024-03-08 22:25:34 -0800

    upload analysis script, correct typos in experiment

---
+++ analysis/analysis.R
@@ -0,0 +1,41 @@
+this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(this.dir)
+
+library(plyr)
+library(dplyr)
+library(reshape)
+library(ggplot2)
+library(gtable)
+library(lme4)
+library(tidyverse)
+library(simr)
+library(lmerTest)
+library(bootstrap)
+library(ggpubr)
+library(broom)    
+library(entropy)
+
+`%notin%` <- Negate(`%in%`)
+
+data <- read.csv("pilot_data.csv")
+data_no_practice <- filter(data, is.na(block_num) == FALSE)
+
+data_no_practice = data_no_practice%>%
+  mutate(new_block = if_else(block_num < 6, block_num, block_num - 5))%>%
+  filter(alt_status == "target")%>%
+  mutate(new_mode = if_else(mode == "a", "target-first", "alternative-first"))
+
+trial_means = data_no_practice %>%
+  group_by(new_block,condition,new_mode) %>%
+  summarise(response = mean(response))
+
+
+satiation_plot1 <- ggplot(data_no_practice, aes(x=new_block, y=response, color = condition, fill = condition)) + 
+  geom_point(data=trial_means,alpha=.9) + 
+  xlab("Presentation Order") +
+  ylab("Acceptability rating")+
+  geom_smooth(method=lm, aes(fill=condition))+theme_bw()+
+  facet_wrap(~new_mode)
+
+satiation_plot1
+
