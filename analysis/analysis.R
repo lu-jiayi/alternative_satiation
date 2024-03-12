2dfe637ad281782493a890df9e21d892c321539d
lu-jiayi <jlg363@stanford.edu>
2024-03-11 19:41:42 -0700

    update analysis script, upload dataset

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -17,7 +17,20 @@ library(entropy)
 
 `%notin%` <- Negate(`%in%`)
 
-data <- read.csv("pilot_data.csv")
+data <- read.csv("data_full.csv")
+
+#Filter out the participants who responded incorrectly more than once to the practice questions:
+
+
+excluded_subjects <- c()
+practice_data=subset(data,item_type %in% c("practice_bad", "practice_good"))
+practice_good_data=subset(practice_data, wrong_attempts <= 1)
+excluded_subjects <- c(excluded_subjects, subset(data, !is.element(workerid, practice_good_data$workerid))$workerid)
+data=subset(data, is.element(workerid, practice_good_data$workerid))
+
+length(unique(data$workerid))
+
+
 data_no_practice <- filter(data, is.na(block_num) == FALSE)
 
 data_no_practice = data_no_practice%>%
@@ -39,10 +52,24 @@ satiation_plot1 <- ggplot(data_no_practice, aes(x=new_block, y=response, color =
 
 satiation_plot1
 
+satiation_plot2 <- ggplot(data_no_practice, aes(x=new_block, y=response, color = new_mode, fill = new_mode)) + 
+  geom_point(data=trial_means,alpha=.9) + 
+  xlab("Presentation Order") +
+  ylab("Acceptability rating")+
+  geom_smooth(method=lm, aes(fill=new_mode))+theme_bw()+
+  facet_wrap(~condition)
+
+satiation_plot2
+
 data_no_practice$new_mode <- as.factor(data_no_practice$new_mode)
+data_no_practice$new_mode <- relevel(as.factor(data_no_practice$new_mode), ref = "alternative-first")
 data_no_practice$condition <- relevel(as.factor(data_no_practice$condition), ref = "GRAM")
 
-contrasts(data_no_practice$new_mode) <- contr.sum(2)
-model <- lmer(response~condition*new_block*new_mode + 
-                (1|workerid), data = data_no_practice)
+data_no_practice = data_no_practice %>%
+  mutate(mode_num = if_else(new_mode == "alternative-first", 0, 1)) %>%
+  mutate(mode_centered = mode_num - mean(mode_num))
+
+model <- lmer(response~condition*new_block*mode_centered + 
+                (1+condition*new_block|workerid) + (1+mode_centered*new_block|item), data = data_no_practice)
 summary(model)
+
